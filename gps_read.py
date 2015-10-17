4bdea673f7294ea37353633aa307f78d3bf9c648
james <jamesmlucas@gmail.com>
2015-10-16 19:39:20 -0500

    Added --no-coords option to gps_read.py
    Makes it easier to only print out altitude, date, etc., if that's
    all you're interested in

--- gps_read.py
+++ gps_read.py
@@ -11,13 +11,16 @@ parser = argparse.ArgumentParser(description='CLI utility for extracting exif GP
 parser.add_argument("-a", "--altitude", action="store_true", default=False, help="include altitude (in feet)")
 parser.add_argument("-i", "--is-tagged", action="store_true", default=False, help="only report whether photo is geo-tagged")
 parser.add_argument("-d", "--date", action="store_true", default=False, help="include photo date")
+parser.add_argument("-n", "--no-coords", action="store_true", default=False, help="don't print coordinates")
 parser.add_argument("-v", "--verbose", action="count", default=0)
 parser.add_argument("file_list", nargs="+")
 
 args = parser.parse_args()
 
 if not args.is_tagged:
-	header = "File Latitude Longitude "
+	header = 'File '
+	if not args.no_coords:
+		header += "Latitude Longitude "
 	if args.altitude:
 		header += "Altitude "
 	if args.date:
@@ -29,15 +32,17 @@ if not args.is_tagged:
 		except Exception as e:
 			print('Failed to open %s: %s' % (i, e))
 		else:
-			latitude, longitude = fi.coordinates()
-			result = "%s %s %s " % (fi.image, latitude, longitude)
+			result = '%s ' % fi.image
+			if not args.no_coords:
+				latitude, longitude = fi.coordinates()
+				result += "%s %s " % (latitude, longitude)
 			if args.altitude: 
 				result += "%s " % fi.altitude()
 			if args.date:
 				result += "%s " % fi.date()
-			print(result)	
 			if args.verbose:
 				fi.get_tags(verbose=args.verbose, stdout=True)
+			print(result)
 else:
 	for i in args.file_list:
                 try:
